d8e0536eefc655a22bf70015e2cc7f251bcba1e1
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-30 22:38:45 +0530

    Create p5.py

---
+++ p5.py
@@ -0,0 +1,6 @@
+s=input()
+l=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XII','XX']
+m=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+a=l.index(s)
+#print result
+print(m[a])
