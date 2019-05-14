520dc95b8c16e843de1be97a40779eb1a651a9d1
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-14 10:26:01 +0530

    Update p63.py

--- p63.py
+++ p63.py
@@ -1,15 +1,12 @@
 n=int(input())
 l3=[]
 s=""
-l4=[]
 l1=list(map(int,input().split()))
 l2=list(map(int,input().split()))
 for i in l1:
-	for j in l2:
-		if i==j:
-			l3.append(i)
-l4=list(set(l3))
-for i in l4:
+	if i in l2 and i not in l3:
+		l3.append(i)
+for i in l3:
 	s=s+str(i)+" "
-#result  
-print(s.strip())
+#result	
+print(s.strip())	
